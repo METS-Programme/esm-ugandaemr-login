d5467ab984c0134df6eb1121ddbbfdad95b64ea9
Dennis Kigen <kigen.work@gmail.com>
2023-08-25 01:33:22 +0300

    (feat) UI enhancements (#4)

--- src/login/login.component.tsx
+++ src/login/login.component.tsx
@@ -2,15 +2,13 @@ import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import {
   Button,
-  Column,
-  Grid,
   InlineLoading,
   InlineNotification,
   PasswordInput,
   TextInput,
   Tile,
 } from "@carbon/react";
-import { EarthFilled, PhoneFilled } from "@carbon/react/icons";
+import { ArrowRight } from "@carbon/react/icons";
 import { useTranslation } from "react-i18next";
 import {
   clearCurrentUser,
@@ -24,6 +22,7 @@ import {
 } from "@openmrs/esm-framework";
 import { performLogin } from "../login.resource";
 import styles from "./login.scss";
+import Logo from "./logo.component";
 
 export interface LoginReferrer {
   referrer?: string;
@@ -40,11 +39,9 @@ const Login: React.FC<LoginReferrer> = () => {
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [isLoggingIn, setIsLoggingIn] = useState(false);
-  const [selectedLocation, setSelectedLocation] = useState(null);
   const passwordInputRef = useRef<HTMLInputElement>(null);
   const usernameInputRef = useRef<HTMLInputElement>(null);
   const formRef = useRef<HTMLFormElement>(null);
-  const showPassword = location.pathname === "/login/confirm";
 
   useEffect(() => {
     if (user) {
@@ -121,147 +118,78 @@ const Login: React.FC<LoginReferrer> = () => {
   if (config.provider.type === "basic") {
     return (
       <div className={`canvas ${styles["container"]}`}>
-        {errorMessage && (
-          <InlineNotification
-            className={styles.errorMessage}
-            kind="error"
-            /**
-             * This comment tells i18n to still keep the following translation keys (used as value for: errorMessage):
-             * t('invalidCredentials')
-             */
-            subtitle={t(errorMessage)}
-            title={t("error", "Error")}
-            onClick={() => setErrorMessage("")}
-          />
-        )}
         <Tile className={styles["login-card"]}>
-          <Grid>
-            <Column sm={4} md={7} lg={8}>
-              <div>{logo}</div>
-            </Column>
-            <Column sm={4} md={7} lg={8}>
-              <form onSubmit={handleSubmit} ref={formRef}>
-                <div className={styles["input-group-custom"]}>
-                  <h4 className={styles["login-text"]}>Login</h4>
-                  <div className={styles["input-container"]}>
-                    <label
-                      htmlFor="username"
-                      className={styles["input-container-label"]}
-                    >
-                      {t("username", "Username")}
-                    </label>
-                    <TextInput
-                      id="username"
-                      type="text"
-                      labelText="username"
-                      name="username"
-                      className={styles["input-text-custom"]}
-                      value={username}
-                      onChange={changeUsername}
-                      ref={usernameInputRef}
-                      hideLabel={true}
-                      autoFocus
-                      required
-                    />
-                  </div>
-                  <div className={styles["input-container"]}>
-                    <label
-                      htmlFor="password"
-                      className={styles["input-container-label"]}
-                    >
-                      {t("password", "Password")}
-                    </label>
-                    <PasswordInput
-                      id="password"
-                      invalidText={t(
-                        "validValueRequired",
-                        "A valid value is required"
-                      )}
-                      className={styles["input-text-custom"]}
-                      textLabel="password"
-                      name="password"
-                      value={password}
-                      onChange={changePassword}
-                      ref={passwordInputRef}
-                      hideLabel={true}
-                      required
-                      showPasswordLabel="Show password"
-                    />
-                  </div>
+          <Logo className={styles.logo} />
+          {errorMessage && (
+            <InlineNotification
+              className={styles.errorMessage}
+              kind="error"
+              /**
+               * This comment tells i18n to still keep the following translation keys (used as value for: errorMessage):
+               * t('invalidCredentials')
+               */
+              subtitle={t(errorMessage)}
+              title={t("error", "Error")}
+              onClick={() => setErrorMessage("")}
+            />
+          )}
+          <form onSubmit={handleSubmit} ref={formRef}>
+            <div className={styles["input-group"]}>
+              <div className={styles["input-container"]}>
+                <TextInput
+                  id="username"
+                  type="text"
+                  name="username"
+                  labelText={t("username", "Username")}
+                  value={username}
+                  onChange={changeUsername}
+                  ref={usernameInputRef}
+                  autoFocus
+                  required
+                />
+              </div>
+              <div style={{ marginTop: "1rem" }} />
+              <div className={styles["input-group"]}>
+                <PasswordInput
+                  id="password"
+                  invalidText={t(
+                    "validValueRequired",
+                    "A valid value is required"
+                  )}
+                  labelText={t("password", "Password")}
+                  name="password"
+                  value={password}
+                  onChange={changePassword}
+                  ref={passwordInputRef}
+                  required
+                  showPasswordLabel="Show password"
+                />
+              </div>
 
-                  <div className={styles["input-container"]}>
-                    <div className={styles["cant-login-text"]}>
-                      {t("cantLogin", "Can't Login?")}
-                    </div>
-                    <div>
-                      <Button
-                        type="submit"
-                        size="md"
-                        className={styles.continueButton}
-                        disabled={!isLoginEnabled || isLoggingIn}
-                      >
-                        {isLoggingIn ? (
-                          <InlineLoading
-                            className={styles.loader}
-                            description={t("loggingIn", "Logging in") + "..."}
-                          />
-                        ) : (
-                          <div>
-                            <span>{t("login", "Login")}</span>
-                          </div>
-                        )}
-                      </Button>
-                    </div>
-                  </div>
-                  <div className={styles["login-card-footer"]}>
-                    <div className={styles["login-card-footer-content"]}>
-                      {" "}
-                      <EarthFilled
-                        className={styles["login-card-footer-icon"]}
-                      />{" "}
-                      {t("visitPortal", "Visit Portal")}{" "}
-                    </div>
-                    <div style={{ flex: "auto" }}>|</div>
-                    <div
-                      className={styles["login-card-footer-content"]}
-                      style={{ flex: "initial" }}
-                    >
-                      {" "}
-                      <PhoneFilled
-                        className={styles["login-card-footer-icon"]}
-                      />{" "}
-                      {t("contactSupport", "Contact Support")}
-                    </div>
-                  </div>
+              <div>
+                <div>
+                  <Button
+                    type="submit"
+                    className={styles.continueButton}
+                    renderIcon={(props) => <ArrowRight size={24} {...props} />}
+                    iconDescription="Log in"
+                    disabled={!isLoginEnabled || isLoggingIn}
+                  >
+                    {isLoggingIn ? (
+                      <InlineLoading
+                        className={styles.loader}
+                        description={t("loggingIn", "Logging in") + "..."}
+                      />
+                    ) : (
+                      <span>{t("login", "Log in")}</span>
+                    )}
+                  </Button>
                 </div>
-              </form>
-            </Column>
-          </Grid>
+              </div>
+            </div>
+          </form>
         </Tile>
-        <div className={styles["footer"]}>
-          <Tile className={styles["powered-by-card"]}>
-            <div className={styles["powered-by-txt"]} style={{ flex: "auto" }}>
-              &copy; {new Date().getFullYear()}{" "}
-              {t("allRightsReserved", "All Rights Reserved")}
-              <span className={styles["text-color-red"]}>
-                {" "}
-                {t(
-                  "MinistryOfHealth",
-                  "Ministry of Health - Republic of Uganda"
-                )}
-              </span>
-            </div>
-            <div className={styles["powered-by-txt"]}>
-              {" "}
-              {t("poweredBy", "Powered by")}:{" "}
-              <span className={styles["text-color-red"]}>
-                {" "}
-                {t("METSProgram", "MakSPH- METS Program")}
-              </span>{" "}
-              | Ver 4.0.0
-            </div>
-          </Tile>
-        </div>
+        <div className={styles["footer"]}></div>
       </div>
     );
   }
