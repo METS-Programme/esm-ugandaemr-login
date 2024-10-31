08d81a77f6828898991e01028652e32fe96bfa3f
Jabar Jeremy <24471994+jabahum@users.noreply.github.com>
2024-10-31 12:35:29 +0300

    update image banner url (#27)

--- src/config-schema.ts
+++ src/config-schema.ts
@@ -67,7 +67,7 @@ export const configSchema = {
   loginBanner: {
     src: {
       _type: Type.String,
-      _default: "/openmrs/spa/banner.png",
+      _default: "/openmrs/spa/banner.svg",
       _description:
         "A path or URL to an image. If null, will use the OpenMRS SVG sprite.",
       _validators: [validators.isUrl],
